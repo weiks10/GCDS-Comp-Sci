f24c7da9743ae4550dc6fc0f19ff1857e7c7f842
weiks10 <lweiksner27@gcds.net>
2025-02-27 10:02:42 -0500

    Update Luke_Weiksner_fixed_length_conversion.py

--- Campbell Comp Sci/Luke_Weiksner_fixed_length_conversion.py
+++ Campbell Comp Sci/Luke_Weiksner_fixed_length_conversion.py
@@ -10,9 +10,9 @@ Log: 1.0 initial release
 
 
 def main():
-    fhand = open('student_data_cs2.txt')                                              #opening the text file
+    fhand = open('student_data_cs2.txt','r')                                              #opening the text file
     with open ('fixed_length_conversion_Luke_Weiksner.csv', 'w') as csvfile:          #opening the file in write as a csv file
         for line in fhand:                                                            #scans each line in file entered                 
-            csvfile.write(line[0:4].rstrip() + "," + line[5:19].rstrip() + "," + line[21:35].rstrip() + "," + line[36:42].rstrip() + "," + line[42:46].rstrip() + "," + line[47:58].rstrip() + "," + line[59:66].rstrip() + "," + line[67:73].rstrip() + "," + line[76:86].rstrip() + "," + line[86:87].rstrip() + "," + line[93:102].rstrip() + "," + line[102:112].rstrip() + "\n")
+            csvfile.write(line[0:4].strip() + "," + line[5:19].strip() + "," + line[21:35].strip() + "," + line[36:42].strip() + "," + line[42:46].strip() + "," + line[47:58].strip() + "," + line[59:66].strip() + "," + line[67:76].strip() + "," + line[76:86].strip() + "," + line[86:87].strip() + "," + line[93:102].strip() + "," + line[102:112].strip() + "\n")
                                                                                       #writes in the csv file with the information split by the bytes and commas.
 main()
